f63496acb677c47c2bd159aa8d7494e44b91c431
lucas <ppas1515@naver.com>
2021-04-22 10:46:26 +0900

    수정

---
+++ Algirithm/src/Student.java
@@ -0,0 +1,27 @@
+
+public class Student {
+	private String name;
+	private String no;
+	public String getName() {
+		return name;
+	}
+	
+	
+	public Student(String name, String no) {
+		super();
+		this.name = name;
+		this.no = no;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	
+}
