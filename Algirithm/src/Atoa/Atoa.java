8bf3ebde0b26b06733d5b01698439abece238664
w4oamI <ppas1515@naver.com>
2021-04-24 21:42:24 +0900

    주석 및 안내문 수정

--- Algirithm/src/Atoa/Atoa.java
+++ Algirithm/src/Atoa/Atoa.java
@@ -15,6 +15,7 @@ public class Atoa {
 		array = input.toCharArray(); //문자열을 chararray로 변환
 		System.out.println(array);
 		
+		//문자열을 배열에 넣은후 하나하나 변환
 		for(int i=0; i<array.length;i++) {
 			if(array[i]>='a' && array[i] <= 'z') {
 				array[i] = (char)(array[i] + ('A'-'a'));  //소문자 a=65 처럼 아스키 코드를 몰라도된다. -> 'A'를 타입캐스팅하면 아스키 코드가 나오기 때문에
@@ -24,6 +25,8 @@ public class Atoa {
 			}
 			System.out.print(array[i]);
 		}
+		System.out.println(" ");
+		System.out.println("끝났습니다.");
 	}
 
 }
