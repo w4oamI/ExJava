7d9f455695eeefefc83671d62a12406ec2f96f06
w4oamI <ppas1515@naver.com>
2021-05-02 18:38:51 +0900

    입력된 숫자의 개수를 출력

---
+++ Algirithm/src/numcount/numcount.java
@@ -0,0 +1,32 @@
+package numcount;
+//입력된 숫자의 개수를 출력
+//---------------------------------------------------------//
+/*
+예제
+입력 : 32158321
+출력 : 
+0:0
+1:2
+2:2
+3:1
+4:0*/
+
+public class numcount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input=3215483;
+		
+		int array [] = new int [input]; //count하기 위한 변수
+		
+		while(input>0) {
+			array[input%10]++;
+			input/=10;
+		}
+		
+		//출력
+		for(int i=0;i<10;i++) {
+			System.out.println(i+":"+array[i]);
+		}
+	}
+}
